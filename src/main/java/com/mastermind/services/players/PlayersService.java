7eebb208daf230f284ca2aedc670232c739e5c02
Biel Simon Rojas <bielsimon2@gmail.com>
2017-10-26 12:52:00 +0200

    Implemented createRandomAIPlayer at PlayersService and refactored existence by name check

--- src/main/java/com/mastermind/services/players/PlayersService.java
+++ src/main/java/com/mastermind/services/players/PlayersService.java
@@ -1,6 +1,8 @@
 package com.mastermind.services.players;
 
 import com.mastermind.model.entities.types.HumanPlayer;
+import com.mastermind.model.entities.types.Player;
+import com.mastermind.model.entities.types.RandomAIPlayer;
 import com.mastermind.model.persistence.RepositoryManager;
 import com.mastermind.model.persistence.repositories.PlayerRepository;
 import com.mastermind.services.players.responses.CreatePlayerResponse;
@@ -14,12 +16,7 @@ public class PlayersService {
     private PlayerRepository playerRepository = RepositoryManager.getPlayerRepository();
     public CreatePlayerResponse<HumanPlayer> createHumanPlayer(String name, String password){
         CreatePlayerResponse<HumanPlayer> response = new CreatePlayerResponse<>();
-        if(playerRepository.findByName(name).isPresent()){
-            response.setSuccess(false);
-            response.getMessages().add(
-                    MessageFormat.format("Player {0} already exists", name));
-            return response;
-        }
+        if (checkExistenceByName(name, response)) return response;
         if(password.length() < 4){
             response.setSuccess(false);
             response.getMessages().add("Password must have at leas 4 characters");
@@ -32,5 +29,24 @@ public class PlayersService {
         response.setCreatedPlayer(playerRepository.save(newPlayer));
         response.setSuccess(true);
         return response;
+    }
+
+    public CreatePlayerResponse<RandomAIPlayer> createRandomAIPlayer(String name, Long seed){
+        CreatePlayerResponse<RandomAIPlayer> response = new CreatePlayerResponse<>();
+        if (checkExistenceByName(name, response)) return response;
+        RandomAIPlayer newPlayer = new RandomAIPlayer();
+        newPlayer.setName(name);
+        newPlayer.setSeed(seed);
+        return response;
+    }
+
+    private boolean checkExistenceByName(String name, CreatePlayerResponse<? extends Player> response) {
+        if(playerRepository.findByName(name).isPresent()){
+            response.setSuccess(false);
+            response.getMessages().add(
+                    MessageFormat.format("Player {0} already exists", name));
+            return true;
+        }
+        return false;
     }
 }
